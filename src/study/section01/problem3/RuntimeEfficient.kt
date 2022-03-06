4b79ad45a0fa441570bd66cb208abb78bad90dbe
Jihun <jihun5000@naver.com>
2022-03-06 19:37:31 +0900

    problem03 solved??

--- src/study/section01/problem3/RuntimeEfficient.kt
+++ src/study/section01/problem3/RuntimeEfficient.kt
@@ -1,5 +1,51 @@
 package study.section01.problem3
 
-fun main() {
+import study.section01.utils.Utils
+import java.util.*
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
+fun main() {
+    //100만개 넘버 생성
+    val numberList = Utils().makeRandomList(1000000)
+
+    measureOriginalSort(numberList)
+
+    measureUsingBitset(numberList)
+}
+
+@OptIn(ExperimentalTime::class)
+fun measureOriginalSort(numberList : List<Int>) {
+    val measuredFunTime = measureTimedValue {
+        numberList.sorted()
+    }
+
+    println("measureOriginalSort - ${measuredFunTime.duration}")
+}
+
+@OptIn(ExperimentalTime::class)
+fun measureUsingBitset(numberList : List<Int>) {
+    val measuredFunTime = measureTimedValue {
+        sortUsingBitset(numberList)
+    }
+
+    println("measureUsingBitwise - ${measuredFunTime.duration}")
+}
+
+fun sortUsingBitset(numberList : List<Int>): List<Int> {
+    val bitset = BitSet(10000000)
+
+    for (i in numberList) {
+        bitset.set(i)
+    }
+
+    //List로 변환
+    val myList = mutableListOf<Int>()
+    for (j in 0 .. bitset.size()) {
+        if (bitset.get(j)) {
+            myList.add(j)
+        }
+    }
+
+    return myList
 }
