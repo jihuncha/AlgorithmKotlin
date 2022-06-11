72c08178f2cd3d3e8693993e1df3dab791283ad1
Jihun <jihun5000@naver.com>
2022-06-12 00:00:21 +0900

    feat: 1254 포기

--- src/algorithm/greedy/back1254.kt
+++ src/algorithm/greedy/back1254.kt
@@ -37,17 +37,15 @@ package algorithm.greedy
 
 //aeaeabaeaeawbx
 
+//abcvwerscba
+
+
 //팰랜드롬인 문자인 경우는 그대로출력
 //팰런드롬이 아닌 경우 검수를 어떻게해야할까
 
 fun main() {
     val input = readln()
-
-//    println(input)
-
     val reverseInput = input.reversed()
-
-//    println(reverseInput)
 
     //팰랜드롬인 경우
     if (input == reverseInput) {
@@ -55,21 +53,49 @@ fun main() {
         return
     }
 
-    println(input.length)
-
     var palindromeLength = 0
-    var backIndex = input.length - 1
 
-    for (i in 0 .. input.length - 1) {
-        if (input[i] == input[backIndex]) {
-            backIndex -= 1
-            palindromeLength += 1
-            println("test $i,  $backIndex , $palindromeLength, ${input[i]}")
-        }
+    for (i in (input.length - 1) downTo  (input.length - 1) / 2) {
+        palindromeLength = Math.max(palindromeLength, checkPalindorome(input, i));
     }
+
+
+//    var backIndex = input.length - 1
+//
+//    for (i in 0 .. input.length - 1) {
+//        if (input[i] == input[backIndex]) {
+//            backIndex -= 1
+//            palindromeLength += 1
+//            println("test $i,  $backIndex , $palindromeLength, ${input[i]}")
+//        }
+//    }
 
     println("pa - $palindromeLength")
 
     println(input.length + (input.length - palindromeLength))
-
 }
+
+fun checkPalindorome(input:String, lastIndex:Int) : Int {
+    var backIndex = lastIndex
+    var result = 0
+    var connect = false
+    var realResult = 0
+
+    for (i in 0 .. lastIndex) {
+        if (input[i] == input[backIndex]) {
+            connect = true
+            backIndex -= 1
+            result += 1
+        } else {
+            if (connect) {
+                connect = false
+                realResult = Math.max(result, realResult)
+                result = 0
+            }
+        }
+    }
+
+    return realResult
+}
+
+//포기기ㅣ이이ㅣ잉
